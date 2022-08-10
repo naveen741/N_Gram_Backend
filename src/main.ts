fe39759bc158ce495f824644a195391df57c0369
Naveen Kumar <naveenkumar.a@tringapps.com>
2022-08-10 16:37:41 +0530

    new server

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
   const appPort = process.env.PORT || 5000;
   await app.listen(appPort);
   console.log(`server running on : 'http://localhost:${appPort}/graphql'`);
